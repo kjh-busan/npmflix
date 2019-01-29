c36e96fac21a708123d14e701efc4c154f18393a
KIM SUNGWON <bit7521@gmail.com>
2019-01-30 01:47:33 +0900

    HomeContainer add

--- npmflix/src/Components/Router.js
+++ npmflix/src/Components/Router.js
@@ -1,21 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
-import Header from "Components/Header";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 
 export default () => (
-    <Router>
-        <>
-        <Header />
-        <Switch>
-            <Route path="/" exact component={Home}/>
-            <Route path="/tv" exact component={TV}/>
-            <Route path="/tv/popular" render={() => <h1>Popular</h1>}/>
-            <Route path="/search" exact component={Search}/>
-            <Redirect from="*" to="/" />
-        </Switch>
-        </>
-    </Router>
+  <Router>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
+  </Router>
 );
